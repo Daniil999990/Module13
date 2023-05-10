3f1f769bef7481c51af93317252fc2aa07680620
Daniil999990 <dannydendi@gmail.com>
2023-05-10 18:30:56 +0300

    Task3

--- Task3/Task3.java
+++ Task3/Task3.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task3 {
 
     public static void main(String[] args) throws IOException {
 
-        int userId = 1; // ідентифікатор користувача
+        int userId = 1;
         String apiUrl = "https://jsonplaceholder.typicode.com/users/" + userId + "/todos";
 
         URL url = new URL(apiUrl);
@@ -26,16 +28,58 @@ public class Task3 {
         }
         in.close();
 
-        System.out.println("Список відкритих задач для користувача з ідентифікатором " + userId + ":");
+        List<Todo> openTodos = new ArrayList<>();
 
-        String[] lines = response.toString().split("\n");
-
-        for (String line : lines) {
-            if (line.contains("\"completed\":false,")) {
-                String title = line.substring(line.indexOf("\"title\":") + 9, line.indexOf(",\"completed\""));
-                System.out.println("- " + title);
+        Todo[] todos = new Gson().fromJson(response.toString(), Todo[].class);
+        for (Todo todo : todos) {
+            if (!todo.isCompleted()) {
+                openTodos.add(todo);
             }
         }
 
+        System.out.println("Список відкритих задач для користувача з ідентифікатором " + userId + ":");
+        for (Todo todo : openTodos) {
+            System.out.println("- " + todo.getTitle());
+        }
+
+    }
+
+    private static class Todo {
+        private int id;
+        private int userId;
+        private String title;
+        private boolean completed;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getUserId() {
+            return userId;
+        }
+
+        public void setUserId(int userId) {
+            this.userId = userId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public boolean isCompleted() {
+            return completed;
+        }
+
+        public void setCompleted(boolean completed) {
+            this.completed = completed;
+        }
     }
 }
